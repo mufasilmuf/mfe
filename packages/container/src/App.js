97103d6b46ab36ece2fe3cb5506dd44f3c8e2806
sosafe-mohamed-mufasil <mohamed.mufasil@external.sosafe.de>
2024-04-08 16:38:30 +0530

    Added invalidation test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/marketingApp';
 const App = () => {
     return (
         <React.Fragment>
-            <h1>Micro Frontend</h1>
+            <h1>Micro Frontend deploy</h1>
             <hr />
             <MarketingApp />
         </React.Fragment>
